87e3ad2360742eedf6da3d8ef93d33643caf8b34
suyuyi <434698964@qq.com>
2020-05-06 20:26:21 +0800

    first commit

---
+++ png_download.py
@@ -0,0 +1,103 @@
+#C:\Users\song_xinbai\Downloads\19-20春季学期资料\结题报告
+import re,requests,time,os,img2pdf,threading,_thread
+import tkinter as tk  # 使用Tkinter前需要先导入
+import tkinter.messagebox
+def single_img_download(path,url):
+    f = open(path,"wb")
+    f.write(requests.get(url).content)
+    f.close()
+class myThread (threading.Thread):
+    def __init__(self, path,url):
+        threading.Thread.__init__(self)
+        self.path = path
+        self.url = url
+    def run(self):
+        single_img_download(self.path, self.url)
+class myThread2(threading.Thread):
+    def __init__(self, path, num):
+        threading.Thread.__init__(self)
+        self.path = path
+        self.num = num
+    def run(self):
+        download_convert_mul(self.path, self.num)
+def download_img(path,ratify_no):
+    url="http://output.nsfc.gov.cn/report/"+str(ratify_no)[:2]+"/"+str(ratify_no)+"_"
+    i=1
+    thread_wait=[]
+    while(requests.get(url+str(i)+".png").status_code!=404):
+        s_path=path+"\\"+str(i)+".png"
+        s_url=url+str(i)+".png"
+        thread=myThread(s_path,s_url)
+        thread.start()
+        thread_wait.append(thread)
+        i+=1
+    for t in thread_wait:
+        t.join()
+    print("download finished")
+def img_to_pdf(path):
+    img_list=[]
+    for i in range(1,len(os.listdir(path))+1):
+        img_name=path+"\\"+str(i)+".png"
+        img_list.append(img_name)
+    f=open(path+"\\result.pdf","wb")
+    pdf_bytes=img2pdf.convert(img_list)
+    f.write(pdf_bytes)
+def download_convert_mul(path,ratify_no):
+    download_img(path,ratify_no)
+    img_to_pdf(path)
+    tkinter.messagebox.showinfo(title="Info", message="目标文件：" + str(ratify_no)+"已完成下载")
+def finished(status,threads):
+    for t in threads:
+        t.join()
+    tkinter.messagebox.showinfo(title="Info", message="目标文件全部已完成下载")
+    status.set("空闲")
+window = tk.Tk()
+window.title('国家自然科学基金共享服务网下载工具')
+window.geometry('600x400')
+canvas = tk.Canvas(window, width=600, height=135, bg='white')
+image_file = tk.PhotoImage(file=r'C:\Users\song_xinbai\Downloads\19-20春季学期资料\结题报告\test\17.png')
+image = canvas.create_image(300, 30, anchor='n', image=image_file)
+canvas.pack(side='top')
+tk.Label(window, text='请输入绝对路径和编号，然后点击下载', font=('Arial', 16)).pack()
+tk.Label(window, text='Path:', font=('Arial', 14)).place(x=10, y=170)
+tk.Label(window, text='No.:', font=('Arial', 14)).place(x=10, y=210)
+tk.Label(window, text='Status:', font=('Arial', 14)).place(x=10, y=250)
+path_str = tk.StringVar()
+path_str.set(r"C:\Users\song_xinbai\Downloads\19-20春季学期资料\结题报告")
+path_input = tk.Entry(window, textvariable=path_str, font=('Arial', 14))
+path_input.place(width=500,x=80, y=170)
+num_str = tk.StringVar()
+num_str.set(60873261)
+num_input = tk.Entry(window, textvariable=num_str, font=('Arial', 14))
+num_input.place(width=500,x=80, y=210)
+status = tk.StringVar()
+status.set('空闲')
+status_output = tk.Entry(window, textvariable=status, font=('Arial', 14))
+status_output.place(width=500,x=80, y=250)
+def download():
+    path = path_input.get()
+    path_str.set("")
+    num_raw = num_input.get()
+    num_str.set("")
+    num_list=str(num_raw).split('、')
+    thread_wait=[]
+    for num in num_list:
+        if(requests.get("http://output.nsfc.gov.cn/conclusionProject/"+str(num)).status_code!=200):
+            tkinter.messagebox.showerror(title="Error",message="无法打开http://output.nsfc.gov.cn/conclusionProject/"+str(num))
+            return
+        if(os.path.exists(path+"\\"+str(num))):
+            tkinter.messagebox.showerror(title="Error",message="目标文件已存在：" + path+"\\"+str(num))
+            return
+        try:
+            dir_path=path+"\\"+str(num)
+            os.makedirs(dir_path)
+            thread=myThread2(dir_path,num)
+            thread.start()
+            status.set(num+"开始下载")
+            thread_wait.append(thread)
+        except BaseException:
+            tkinter.messagebox.showerror(title="Error",message="无法创建路径" + str(dir_path))
+tk.Label(window, text='注意：下载多个文件时，请用‘、’分割编号', font=('Arial', 10)).place(y=330)
+btn_login = tk.Button(window, text='下载', command=download)
+btn_login.place(width=50,x=275, y=290)
+window.mainloop()
